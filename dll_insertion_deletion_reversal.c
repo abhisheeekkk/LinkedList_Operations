32162111c22c3dec09bdc2cadf5a67bffbce0b6d
Abhishek <abhishekshukla9586@gmail.com>
2024-04-25 20:20:29 +0530

    reversing dll

--- dll_insertion_deletion_reversal.c
+++ dll_insertion_deletion_reversal.c
@@ -1,10 +1,4 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+//Author - Abhishek
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,6 +10,7 @@ struct node
     struct node* prev;
 };
 
+//-----------------INSERTION----------------------------
 void insertAtStart(struct node** head, int data)
 {
     struct node* tmp = (struct node*)malloc(sizeof(struct node));
@@ -83,7 +78,99 @@ void insertAfterNode(struct node* head, int data, int afterData)
     
 }
 
+//---------------------REVERSAL-----------------------------
 
+void reverseLL(struct node** head)
+{
+    struct node* curr = *head;
+    struct node* prv = NULL;
+    struct node* nxt = NULL;
+    
+    //lets consider ll [4->3->2->1->5]
+    //so curr initially points to 4.
+    //we store 3 in the nxt variable
+    //we make the next pointer variable point to prv, in first 
+    //iteration to NULL, we point prev pointer to next element
+    //then we make prv variable as curr.
+    //and then curr variable will move to next member i.e. 3. again we repeat every thing.
+    while(curr != NULL)
+    {
+        nxt         = curr->next;
+        curr->next  = prv;
+        curr->prev  = nxt;
+        prv         = curr;
+        curr        = nxt;
+    }
+    
+    *head = prv;
+}
+
+
+//---------------------DELETION-----------------------------
+void deleteNode(struct node** head, int position)
+{
+    struct node* prv = NULL;
+    struct node* tmp = NULL;
+    
+    prv = *head;
+    tmp = *head;
+    
+    for(int i = 0 ; i < position ; ++i)
+    {
+        //we want to delete first position
+        if((i == 0) && (position == 1))
+        {
+            //try to visualize, very simple, we simply move the 
+            //head from first node to second node, so we change the 
+            //pointer to the next node.
+            //in this case we also want to change the next pointer's
+            //previous member to NULL, since now 2nd node is first node.
+            (*head) = (*head)->next;
+            (*head)->next->prev = NULL;
+            
+            //either free tmp or prv, basically both are pointing to same memory.
+            free(prv);
+            return;
+        }
+        //first look at the else condition, we have reached the position that we want to remove
+        if(i == position - 1)
+        {
+            //now here also try to visualize, lets say this is the ll [4->3->2->1->5->10]
+            //we want to remove the position no. 5 which is node 5.
+            //now currently prv is pointing to 1 & tmp is pointing to 5.
+            //so first we want to make sure that prv's next is updated to tmp->next, cuz
+            //node 5 will be removed.
+            //Also we want to make sure that node 10's previous is pointing to the prv node.
+            prv->next = tmp->next;
+            
+            //this condition is necessary if we want to remove the last node our ll is [4->3->2->1->5->10]
+            //now we want to remove 10 from the ll.
+            //prv is pointing to 5, and tmp is pointing to 10
+            //above we have made sure that prv's next is pointing to temp's next which will be NULL sice it's 
+            //last node.
+            //But we don't have to update the temp's next node's prev pointer since there is no node.
+            if(tmp->next != NULL)
+                tmp->next->prev = prv;
+            
+            //free the memory so it can be reused!!!
+            free(tmp);
+        }
+        else        //here we keep assigning the prv pointer to current nodes previous node and keep moving tmp to next node.
+        {
+            prv = tmp;
+            
+            //the position was greater than the size of ll
+            if(prv == NULL)
+                return;
+                
+            tmp = tmp->next;
+        }
+    }
+    
+}
+
+
+//---------------------PRINTING-----------------------------
 void printList(struct node* head)
 {
     while(head->next != NULL)
@@ -112,6 +199,14 @@ int main()
     insertAfterNode(head, 10, 5);
     printList(head);
     printf("\n");
+    
+    deleteNode(&head, 6);
+    printList(head);
+    printf("\n");
+    
+    reverseLL(&head);
+    printList(head);
+    printf("\n"); 
     
     return 0;
 }
--- sll_insertion_reversal.c
+++ sll_insertion_reversal.c
@@ -1,3 +1,5 @@
+//Author - Abhishek
+
 #include <stdio.h>
 #include <stdlib.h>
 
